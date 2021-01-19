2f3f821d855c6b8da9f5e07307826440aaef6fd6
Vitoriano <vitorianoernandes2012@gmail.com>
2021-01-18 23:24:30 -0300

    configurando o eslint e o typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
+
+
+const flo = 'ba';
 
 export default function Home() {
   return (
